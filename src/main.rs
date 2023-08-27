f469281464ce871cfa48e695b8bf2754de447020
pickled-dict <mathew_forgione@proton.me>
2023-08-26 21:09:42 -0300

    initial implementation of the string generate function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use available_chars::AvailableChars;
 use chars_options::CharsOptions;
+use rand::seq::SliceRandom;
+
 mod available_chars;
 mod chars_options;
 
-fn main() {
-    let options = CharsOptions {upper: true, lower: false, numbers: true, symbols: false};
-    let available_with_options = AvailableChars::builder().from_options(options).build();
+fn generate_string(available: AvailableChars, size: usize) -> String {
+    (0..size).map(|_| available.chars.choose(&mut rand::thread_rng()).unwrap())
+        .collect()
+}
 
-    println!("{:?}", available_with_options.chars);
+fn main() {
+    let options = CharsOptions {upper: true, lower: false, numbers: false, symbols: false};
+    let available = AvailableChars::builder().from_options(options).build();
+
+    let generated = generate_string(available, 50);
+
+    println!("{}", generated);
 }
