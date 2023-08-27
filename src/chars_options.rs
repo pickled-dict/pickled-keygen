fead7f637e20abda310c6d18241b131169ac2698
pickled-dict <mathew_forgione@proton.me>
2023-08-27 00:54:50 -0300

    added tests for chars_options.rs

--- src/chars_options.rs
+++ src/chars_options.rs
@@ -11,3 +11,74 @@ impl CharsOptions {
         self.numbers == false && self.lower == false && self.symbols == false && self.upper == false
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_empty_returns_true_if_empty() {
+        let options = CharsOptions {
+            upper: false,
+            lower: false,
+            numbers: false,
+            symbols: false,
+        };
+        assert!(options.is_empty());
+    }
+
+    #[test]
+    fn test_is_empty_returns_false_if_upper_is_true() {
+        let options = CharsOptions {
+            upper: true,
+            lower: false,
+            numbers: false,
+            symbols: false,
+        };
+        assert!(!options.is_empty());
+    }
+
+    #[test]
+    fn test_is_empty_returns_false_if_lower_is_true() {
+        let options = CharsOptions {
+            upper: false,
+            lower: true,
+            numbers: false,
+            symbols: false,
+        };
+        assert!(!options.is_empty());
+    }
+
+    #[test]
+    fn test_is_empty_returns_false_if_numbers_is_true() {
+        let options = CharsOptions {
+            upper: false,
+            lower: false,
+            numbers: true,
+            symbols: false,
+        };
+        assert!(!options.is_empty());
+    }
+
+    #[test]
+    fn test_is_empty_returns_false_if_symbols_is_true() {
+        let options = CharsOptions {
+            upper: false,
+            lower: false,
+            numbers: false,
+            symbols: true,
+        };
+        assert!(!options.is_empty());
+    }
+
+    #[test]
+    fn test_is_empty_returns_false_if_all_are_true() {
+        let options = CharsOptions {
+            upper: true,
+            lower: true,
+            numbers: true,
+            symbols: true,
+        };
+        assert!(!options.is_empty());
+    }
+}
